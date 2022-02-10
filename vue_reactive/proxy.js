a56ddb0f699c391bec0dac9c5b77883a237dfca2
zhuanghaixin <649308293@qq.com>
2022-02-10 23:14:50 +0800

    upd: 极客时间 响应式数据 defineProperty和proxy的区别

---
+++ vue_reactive/proxy.js
@@ -0,0 +1,28 @@
+let getDouble = n => n * 2
+let obj = {}
+let count = 1
+let double = getDouble(count)
+
+let proxy = new Proxy(obj, {
+  get: function (target, prop) {
+    return target[prop]
+  },
+  set: function (target, prop, value) {
+    target[prop] = value
+    if (prop === 'count') {
+      double = getDouble(value)
+    }
+  },
+  deleteProperty (target, prop) {
+    delete target[prop]
+    if (prop === 'count') {
+      double = NaN
+    }
+  }
+})
+console.log(obj.count, double)
+proxy.count = 2
+console.log(obj.count, double)
+delete proxy.count
+// // 删除属性后，我们打印log时，输出的结果就会是 undefined NaN
+console.log(obj.count, double)
