46e80b34b8086335478181a1062880b1450551f1
zhuanghaixin <649308293@qq.com>
2022-02-13 15:16:43 +0800

    upd: 极客时间 组件化 星级组件 组件事件

---
+++ geek-admin/src/components/Rate1.vue
@@ -0,0 +1,63 @@
+
+<template>
+    <div :style="fontStyle">
+        <div class="rate" @mouseout="mouseOut">
+            <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">☆</span>
+            <span class="hollow" :style="fontwidth">
+                <span @mouseover="mouseOver(num)" v-for="num in 5" :key="num">★</span>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, computed, ref } from 'vue';
+let props = defineProps({
+    value: Number,
+    theme: {
+        type: String,
+        default: 'orange'
+    }
+})
+let width = ref(props.value);
+function mouseOver(i) {
+    console.log('mouseOVer')
+    console.log(i)
+    width.value = i
+    console.log('width.value===', width.value)
+}
+function mouseOut() {
+
+    width.value = props.value
+    console.log('width.value', width.value)
+    console.log('props.value', props.value)
+}
+const themeObj = {
+    'black': '#00',
+    'white': '#fff',
+    'red': '#f5222d',
+    'orange': '#fa541c',
+    'yellow': '#fadb14',
+    'green': '#73d13d',
+    'blue': '#40a9ff',
+}
+let fontStyle = computed(() => {
+
+    return `color:${themeObj[props.theme]}`
+})
+const fontwidth = computed(() => `width:${width.value}em;`)
+</script>
+<style scoped>
+.rate {
+    position: relative;
+    display: inline-block;
+}
+.rate > span.hollow {
+    position: absolute;
+    display: inline-block;
+    top: 0;
+    left: 0;
+    width: 0;
+    overflow: hidden;
+}
+</style>
--- geek-admin/src/pages/component.vue
+++ geek-admin/src/pages/component.vue
@@ -2,11 +2,19 @@
     <h1>组件化</h1>
     <Rate :value="5"></Rate>
     <Rate theme="blue" :value="2"></Rate>
-    <Rate theme="red" :value="4"></Rate>
+    <Rate theme="red" :value="4"></Rate>================================================
+    <Rate1 :value="3.5"></Rate1>
+    <!-- <h1>你的评分是{{ score }}</h1>
+    <Rate1 :value="score" @update-rate="update"></Rate1>-->
 </template>
 
 <script setup>
 
 import Rate from '../components/Rate.vue'
-console.log(Rate)
+import Rate1 from '../components/Rate1.vue'
+// let score = ref(3.5)
+// function update(num) {
+//     score.value = num
+// }
+
 </script>
