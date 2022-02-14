247998ca1f114245f490c75c81b44180ee4c5d7b
zhuanghaixin <649308293@qq.com>
2022-02-14 22:53:47 +0800

    upd: 极客时间 vuex 实现mini-vuex

--- vuex-demo/src/components/Count.vue
+++ vuex-demo/src/components/Count.vue
@@ -3,7 +3,8 @@
 </template>
 <script setup>
 import { computed } from 'vue'
-import { useStore } from 'vuex'
+// import { useStore } from 'vuex'
+import { useStore } from '../store/gvuex'
 // store获取数据源
 let store = useStore()
 let count = computed(() => store.state.count)
--- vuex-demo/src/store/index.js
+++ vuex-demo/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'vuex'
+// import { createStore } from 'vuex'
+import { createStore } from './gvuex'
 // 创建一个数据存储
 const store = createStore({
   state () {
